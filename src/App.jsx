deb2cd85f80a2428e195184246ec000d71408678
PSCoder10462 <pscoder10462@gmail.com>
2021-05-15 14:49:22 +0530

    react router dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./assets/css/App.css";
 import Gameboard from "./components/Gameboard";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Gameboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Header />
+          </Route>
+          <Route path="/play">
+            <Gameboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
